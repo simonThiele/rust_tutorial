56698d77b82cbe000f9bca4fbc9f09eaa9f9544d
Simon Thiele <simon.thiele.st@googlemail.com>
2017-08-09 20:19:25 +0200

    basic crate example

---
+++ 03_get_the_number/get_the_number/src/main.rs
@@ -0,0 +1,34 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    // you can write let here beacuse rust is able to get the type on compile time <3
+    let secret = rand::thread_rng().gen_range(1, 101);
+    println!("secret: {}", secret);
+
+
+    // mut = mutable. props are immutable by default <3>
+    let mut input = String::new();
+    println!("Type a number:");
+
+    io::stdin().read_line(&mut input)
+        .ok()
+        .expect("Error while reading the input");
+
+    // input => int
+    let input: u32 = input.trim().parse()
+        .ok()
+        .expect("please type a number!");
+
+    println!("input: {}", input);
+
+
+    match input.cmp(&secret) {
+        Ordering::Less => println!("to small"),
+        Ordering::Greater => println!("to big"),
+        Ordering::Equal => println!("bam!")
+    }
+}
