3a80110e9228006a33a004d29c3932c1e39318f6
Simon Thiele <simon.thiele.st@googlemail.com>
2017-09-04 20:39:20 +0200

    add iterators example

---
+++ 05_iterators/iterators/src/main.rs
@@ -0,0 +1,50 @@
+fn main() {
+    println!("the hard way");
+    the_hard_way();
+
+    println!("the sugar way");
+    for_suggar();
+
+    println!("collectors");
+    collect();
+}
+
+fn the_hard_way() {
+    let mut range = 0..10;
+    loop {
+        match range.next() {
+            Some(x) => {
+                println!("{}", x);
+            }
+            None => { break }
+        }
+    }
+}
+
+fn for_suggar() {
+    let numbers = vec![1, 2, 3 ,4];
+    for i in 0..numbers.len() {
+        println!("{}", i);
+    }
+
+    for item in &numbers {
+        println!("{}", item);
+    }
+}
+
+fn collect() {
+    println!("sum");
+    let all = (0..101).collect::<Vec<_>>();
+    println!("{:?}", all);
+    
+    println!("find");
+    let matches = (0..101).find(|x| *x > 42);
+    match matches {
+        Some(x) => println!("found: {}", x),
+        None => println!("nope")
+    }
+
+    println!("reduce");
+    let res = (0..11).fold(0, |sum, x | x + sum);
+    println!("{}", res);
+}
